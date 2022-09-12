fceae442f9b70ad1c16af478e4401da82d1e9b93
junseowon <junseowon@gmail.com>
2022-09-12 09:29:21 +0900

    image_setting

--- code/main.py
+++ code/main.py
@@ -10,21 +10,21 @@ SURFACE = pygame.display.set_mode((surface_width, surface_height))
 pygame.display.set_caption('hiihhi')
 FPSCLOCK = pygame.time.Clock()
 
-#Howtoplay를 위한 이미지들!...
-back_button = ("E:/gongmo/image/surface/Back_button.png")
-back_button_act = ("E:/gongmo/image/surface/Back_button_act.png")
+#Howtoplay를 위한 이미지들!
+back_button = ("image/surface/Back_button.png")
+back_button_act = ("image/surface/Back_button_act.png")
 
 #play를 위한 이미지들!
 #background_image = ("") #만들기
 
 #main_menu,play 캐릭터들
-sprite_img = pygame.image.load("E:/gongmo/image/characters/sprite.PNG")
-ham_zzizzi = pygame.image.load("E:/gongmo/image/characters/hamster_zzizzi_success.png")
-start_button = pygame.image.load("E:\gongmo\image\surface/start_button.png")
-start_button_act = pygame.image.load("E:/gongmo/image/surface/start_button_clicked.png")
-howtoplay_button = pygame.image.load("E:\gongmo\image\surface/howtoplay_button.png")
-howtoplay_button_act = pygame.image.load("E:/gongmo/image/surface/howtoplay_button_act.png") 
-penguin_smile = pygame.image.load("E:/gongmo/image/characters/penguin_smile.png")
+sprite_img = pygame.image.load("image/characters/sprite.PNG")
+ham_zzizzi = pygame.image.load("image/characters/hamster_zzizzi_success.png")
+start_button = pygame.image.load("image/surface/start_button.png")
+start_button_act = pygame.image.load("image/surface/start_button_clicked.png")
+howtoplay_button = pygame.image.load("image/surface/howtoplay_button.png")
+howtoplay_button_act = pygame.image.load("image/surface/howtoplay_button_act.png") 
+penguin_smile = pygame.image.load("image/characters/penguin_smile.png")
 #robby = pygame.image.load("E:/gongmo/image/surface/Robby.png") //만들어야함!
 
 class Button():
@@ -42,15 +42,15 @@ class Button():
             
 def main_menu():
     
-    font = pygame.font.Font("E:/gongmo/font/DungGeunMo.ttf", 100) #폰트적용안됨
-    game_title = font.render("ㅋ", True, (0,0,0)) #안됨
+    font = pygame.font.Font("font/DungGeunMo.ttf", 100)
+    game_title = font.render("ㅋ", True, (0,0,0))
     
     while True:
         for event in pygame.event.get():
             SURFACE.fill('#5CACEE') 
             SURFACE.blit(game_title, (430, 150))
                 
-            Button(howtoplay_button, 512, 420, 256, 144, howtoplay_button_act, 512,420, howtoplay)
+            Button(howtoplay_button, 512, 420, 256, 144, howtoplay_button_act, 512,420, Explain_game)
                 
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -58,13 +58,13 @@ def main_menu():
         pygame.display.update()
         FPSCLOCK.tick(30)
 
-def howtoplay():
+def Explain_game():
     #font = pygame.font.Font("ㅋ")
     while True:
         for event in pygame.event.get():
             SURFACE.fill('#FFAEB9')
             
-            Button(start_button, 1000, 560, 256, 144, start_button_act, 1000, 560, play)#에러원인
+            Button(start_button, 1000, 560, 256, 144, start_button_act, 1000, 560, main_game)
             
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -72,7 +72,7 @@ def howtoplay():
         pygame.display.update()
         FPSCLOCK.tick(30)
  
-def play():
+def main_game():
     x_move = 0
     y_move = 0
     x_pos = 0
@@ -102,17 +102,14 @@ def play():
                     x_pos = 0
                 elif event.key == pygame.K_d:
                     x_pos = 0
-            
-            SURFACE.fill('#00B9FF')
-            SURFACE.blit(sprite_img, (x_move, y_move))
-    
+
         x_move += x_pos
         y_move += y_pos
 
         SURFACE.fill('#00B9FF')
         SURFACE.blit(sprite_img, (x_move, y_move))
 
-        pygame.display.update() #howtoplay가 먼저 사라지는 원인
+        pygame.display.update()
         FPSCLOCK.tick(30)
 
 main_menu()
